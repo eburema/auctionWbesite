e016ac7baeb5241dc6cf1c78550cc01efd9aced8
Erwin Burema <e.burema@gmail.com>
2019-03-01 12:47:36 +0100

    Bug fixes, gallery is now sorted

--- lib/auctionWbesite_web/controllers/gallery_controller.ex
+++ lib/auctionWbesite_web/controllers/gallery_controller.ex
@@ -1,11 +1,16 @@
 defmodule AuctionWbesiteWeb.GalleryController do
+  import Ecto.Query
   use AuctionWbesiteWeb, :controller
   alias AuctionWbesite.Repo, as: Repo
   alias AuctionWbesite.Artist, as: Artist
   alias AuctionWbesite.Art, as: Art
 
   def index(conn, _params) do
-    data = Repo.all(Art) |> Repo.preload(:artist)
+  
+    query = from u in Art, order_by: u.artist_id, order_by: u.title
+        
+    data = Repo.all(query) |> Repo.preload(:artist)
+    
     conn
     |> assign(:data, data)
     |> assign(:current_id, AuctionWbesite.Agent.get_art_id)
